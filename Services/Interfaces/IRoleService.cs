4716321bebfe788a0d9aeb9e4f105ec1da7871c6
AtheeshRathnaweera <atheesh.rathnaweera@novigi.com.au>
2025-01-11 03:10:11 +0530

    refactor: improve code readability and formatting

--- Services/Interfaces/IRoleService.cs
+++ Services/Interfaces/IRoleService.cs
@@ -1,13 +1,12 @@
-using LibraCore.Backend.Entities;
 using LibraCore.Backend.Models;
 
-namespace LibraCore.Backend.Services;
+namespace LibraCore.Backend.Services.Interfaces;
 
 public interface IRoleService
 {
-    Task<RoleModel?> GetAsync(int id);
-    Task<IEnumerable<RoleModel>> GetAllAsync();
-    Task<RoleModel> CreateAsync(RoleModel roleModel);
-    Task<RoleModel> UpdateAsync(int id, RoleModel roleModel);
-    Task<bool> DeleteAsync(int id);
+  Task<RoleModel?> GetAsync(int id);
+  Task<IEnumerable<RoleModel>> GetAllAsync();
+  Task<RoleModel> CreateAsync(RoleModel roleModel);
+  Task<RoleModel> UpdateAsync(int id, RoleModel roleModel);
+  Task<bool> DeleteAsync(int id);
 }
