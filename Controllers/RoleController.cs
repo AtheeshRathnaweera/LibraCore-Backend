9ce0b0905839af7fea50ff742a635214fecca8bd
AtheeshRathnaweera <atheesh.rathnaweera@novigi.com.au>
2025-01-10 16:59:22 +0530

    feat: remove try-catch blocks, utilizing custom exception handling middleware

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -21,23 +21,15 @@ public class RoleController : ControllerBase
   [HttpGet]
   public async Task<ActionResult<IEnumerable<RoleModel>>> GetAll()
   {
-    try
-    {
-      var roles = await _roleService.GetAllAsync();
+    var roles = await _roleService.GetAllAsync();
 
-      if (!roles.Any())
-      {
-        _logger.LogInformation("No roles found.");
-        return NotFound("No roles available.");
-      }
-
-      return Ok(roles);
-    }
-    catch (Exception ex)
+    if (!roles.Any())
     {
-      _logger.LogError(ex, "Error occurred while fetching roles");
-      return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+      _logger.LogInformation("No roles found.");
+      return NotFound("No roles available.");
     }
+
+    return Ok(roles);
   }
 
   [HttpGet("{id}")]
@@ -49,7 +41,6 @@ public class RoleController : ControllerBase
       return BadRequest("Invalid ID provided.");
     }
 
-    try
     {
       var role = await _roleService.GetAsync(id);
       if (role == null)
@@ -58,11 +49,6 @@ public class RoleController : ControllerBase
         return NotFound("Role not found.");
       }
       return Ok(role);
-    }
-    catch (Exception ex)
-    {
-      _logger.LogError(ex, "Error occurred while fetching role with ID: {RoleId}", id);
-      return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
     }
   }
 
@@ -75,7 +61,6 @@ public class RoleController : ControllerBase
       return BadRequest("Role name is required.");
     }
 
-    try
     {
       var newRole = new RoleModel(name: createRoleRequest.Name);
       var createdRole = await _roleService.CreateAsync(newRole);
@@ -86,11 +71,6 @@ public class RoleController : ControllerBase
       // It includes the URI of the newly created resource (using the GetById action) in the Location header 
       // along with the created role object in the response body.
       return CreatedAtAction(nameof(GetById), new { id = createdRole.Id }, createdRole);
-    }
-    catch (Exception ex)
-    {
-      _logger.LogError(ex, "An error occurred while creating a role with Name: {Name}", createRoleRequest.Name);
-      return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred while creating the role.");
     }
   }
 
@@ -103,51 +83,35 @@ public class RoleController : ControllerBase
       return BadRequest("Invalid update request.");
     }
 
-    try
+    if (string.IsNullOrWhiteSpace(updateRoleRequest.Name))
     {
-      if (string.IsNullOrWhiteSpace(updateRoleRequest.Name))
-      {
-        _logger.LogWarning("Role name is missing for update: {RoleId}", id);
-        return BadRequest("Role name cannot be empty.");
-      }
-
-      var roleWithUpdates = new RoleModel { Id = id, Name = updateRoleRequest.Name };
-      var updatedRole = await _roleService.UpdateAsync(id, roleWithUpdates);
-
-      if (updatedRole == null)
-      {
-        _logger.LogWarning("Role not found for update: {RoleId}", id);
-        return NotFound($"Role with ID {id} not found.");
-      }
-
-      return Ok(updatedRole);
+      _logger.LogWarning("Role name is missing for update: {RoleId}", id);
+      return BadRequest("Role name cannot be empty.");
     }
-    catch (Exception ex)
+
+    var roleWithUpdates = new RoleModel { Id = id, Name = updateRoleRequest.Name };
+    var updatedRole = await _roleService.UpdateAsync(id, roleWithUpdates);
+
+    if (updatedRole == null)
     {
-      _logger.LogError(ex, "Error occurred while updating the role: {RoleId}", id);
-      return StatusCode(StatusCodes.Status500InternalServerError, "Error occurred while updating the role.");
+      _logger.LogWarning("Role not found for update: {RoleId}", id);
+      return NotFound($"Role with ID {id} not found.");
     }
+
+    return Ok(updatedRole);
   }
 
   [HttpDelete("{id}")]
   public async Task<ActionResult> DeleteRole(int id)
   {
-    try
-    {
-      var result = await _roleService.DeleteAsync(id);
+    var result = await _roleService.DeleteAsync(id);
 
-      if (!result)
-      {
-        _logger.LogWarning("Role with id {Id} not found.", id);
-        return NotFound($"Role with id {id} not found.");
-      }
-
-      return NoContent(); // 204 No Content (successfully deleted)
-    }
-    catch (Exception ex)
+    if (!result)
     {
-      _logger.LogError(ex, "An error occurred while deleting the role.");
-      return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+      _logger.LogWarning("Role with id {Id} not found.", id);
+      return NotFound($"Role with id {id} not found.");
     }
+
+    return NoContent(); // 204 No Content (successfully deleted)
   }
 }
