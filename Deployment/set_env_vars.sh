74824f936a0e4ba411aa9e63e8779d839dc569a8
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-01-21 17:11:54 +0530

    feat: add Auth0 environment variables to set_env_vars.sh for authentication configuration

--- Deployment/set_env_vars.sh
+++ Deployment/set_env_vars.sh
@@ -8,4 +8,7 @@
 export MYSQL_LOCAL_USER="replace_with_mysql_username"
 export MYSQL_LOCAL_PASSWORD="replace_with_mysql_password"
 
+export AUTH0_DOMAIN="replace_with_auth0_domain"
+export AUTH0_AUDIENCE="replace_with_auth0_audience"
+
 echo "Environment variables set successfully."
