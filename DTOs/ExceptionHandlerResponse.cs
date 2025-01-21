b2ea51e12e02e9bef7d3b216dc109cf21c3a5dfa
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-01-21 17:11:54 +0530

    feat: remove statusCode property from ExceptionHandlerResponse

--- DTOs/ExceptionHandlerResponse.cs
+++ DTOs/ExceptionHandlerResponse.cs
@@ -2,7 +2,6 @@ namespace LibraCore.Backend.DTOs;
 
 public class ExceptionHandlerResponse
 {
-  public int StatusCode { get; set; }
   public required string Message { get; set; }
   public DateTime Timestamp { get; set; }
   public ExceptionDebugInfo? Debug { get; set; }
