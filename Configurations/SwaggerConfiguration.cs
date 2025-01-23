5a60da02c52982334eb563c9be01db34802dcca7
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-01-24 01:42:58 +0530

    feat: enhance Swagger configuration with JWT Bearer authentication and XML comments

--- Configurations/SwaggerConfiguration.cs
+++ Configurations/SwaggerConfiguration.cs
@@ -12,6 +12,7 @@ public static class SwaggerConfiguration
       var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
       var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
+      // Add Swagger document information
       options.SwaggerDoc("v1", new()
       {
         Title = "LibraCore API V1",
@@ -24,7 +25,31 @@ public static class SwaggerConfiguration
           Url = new Uri("https://opensource.org/licenses/MIT")
         }
       });
+
+      // Include XML comments in Swagger
       options.IncludeXmlComments(xmlPath);
+
+      // Add security definition for JWT Bearer authentication
+      options.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+      {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme. The token must be generated using an Auth0 client.",
+        In = ParameterLocation.Header,
+      });
+
+      // Add security requirement for JWT Bearer authentication
+      options.AddSecurityRequirement(new OpenApiSecurityRequirement
+      {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
+            },
+            new string[] {}
+        }
+      });
     });
   }
 }
