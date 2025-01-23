8a40fe1adbc90b68d36fb85782d39b94fa903506
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-01-23 11:30:35 +0530

    refactor: reformat the code

--- Services/Interfaces/IRoleService.cs
+++ Services/Interfaces/IRoleService.cs
@@ -5,8 +5,12 @@ namespace LibraCore.Backend.Services.Interfaces;
 public interface IRoleService
 {
   Task<RoleModel?> GetAsync(int id);
+
   Task<IEnumerable<RoleModel>> GetAllAsync();
+
   Task<RoleModel> CreateAsync(RoleModel roleModel);
+
   Task<RoleModel> UpdateAsync(int id, RoleModel roleModel);
+  
   Task<bool> DeleteAsync(int id);
 }
