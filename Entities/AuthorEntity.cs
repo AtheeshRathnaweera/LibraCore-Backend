ef2a2ff38aa29d63946c87b27cdd50c1a520a1f7
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-01-25 23:57:33 +0530

    feat: create the Author entity class

---
+++ Entities/AuthorEntity.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using LibraCore.Backend.Entities.Base;
+
+namespace LibraCore.Backend.Entities;
+
+[Table("author")]
+public class AuthorEntity : BaseEntity
+{
+  public int Id { get; set; }
+
+  [Column("first_name")]
+  public required string FirstName { get; set; }
+
+  [Column("last_name")]
+  public required string LastName { get; set; }
+
+  [Column("date_of_birth")]
+  public DateTime DateOfBirth { get; set; }
+
+  [Column("date_of_death")]
+  public DateTime DateOfDeath { get; set; }
+
+  public required string Nationality { get; set; }
+
+  public string ImageUrl { get; set; }
+}
